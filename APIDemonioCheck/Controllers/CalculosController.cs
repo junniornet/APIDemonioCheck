900ee9a8435223f150971bc0a3eb78e980b2238a
Junnior Villanueva <junnior.com.pe@outlook.com>
2025-07-12 21:02:24 -0500

    Agregar archivos de proyecto.

---
+++ APIDemonioCheck/Controllers/CalculosController.cs
@@ -0,0 +1,27 @@
+﻿using APIDemonioCheck.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIDemonioCheck.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculosController : ControllerBase
+    {
+        // POST: CalculosController/Edit/5
+        [HttpPost]
+        public ActionResult Calculos(Maquina maquina)
+        {
+            try
+            {
+                return Ok(maquina);
+            }
+            catch
+            {
+
+            }
+            return Ok();
+        }
+
+    }
+}
